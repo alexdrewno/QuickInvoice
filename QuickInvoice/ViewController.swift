8b009c0879857cd21ba7842c55becb39ff299e61
Alex Drewno <adrewno14@gmail.com>
2020-07-29 22:57:52 -0500

    Initial Commit

---
+++ QuickInvoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickInvoice
+//
+//  Created by Alex Drewno on 7/29/20.
+//  Copyright © 2020 Alex Drewno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QuickInvoiceTests/QuickInvoiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuickInvoiceTests.swift
+//  QuickInvoiceTests
+//
+//  Created by Alex Drewno on 7/29/20.
+//  Copyright © 2020 Alex Drewno. All rights reserved.
+//
+
+import XCTest
+@testable import QuickInvoice
+
+class QuickInvoiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
